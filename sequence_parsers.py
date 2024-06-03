9fc978a6643d5f1edd55026a6b19b007065804c9
DatPill <smartcrackerprezents@gmail.com>
2024-06-03 23:13:54 +0300

    Implement parse() method + add some validation

--- sequence_parsers.py
+++ sequence_parsers.py
@@ -1,9 +1,34 @@
-class BasicParser:
-    _entries = 1
+from Bio import Entrez, SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature
 
-    def __init__(self, ncbi_id: int = None, organism: str = None, gene: str = None):
+
+class BasicParser:
+    def __init__(
+            self,
+            ncbi_id: int = None,
+            organism: str = None,
+            gene: str = None,
+            entries: int = 1
+        ):
+
+        # TODO: validate on initialization
         self._ncbi_id = f'txid{ncbi_id}' if ncbi_id else None
         self._organism = f'"{organism}"' if organism else None
+        self.gene = gene
+        self.entries = entries
+
+    @staticmethod
+    def organism_valid(organism_name: str) -> bool:
+        if isinstance(organism_name, str) and ' ' in organism_name:
+            return True
+        return False
+
+    @staticmethod
+    def ncbi_id_valid(ncbi_id: int) -> bool:
+        if isinstance(ncbi_id, int) and ncbi_id > 0:
+            return True
+        return False
 
     @property
     def ncbi_id(self):
@@ -11,7 +36,7 @@ class BasicParser:
 
     @ncbi_id.setter
     def ncbi_id(self, value):
-        if isinstance(value, int) and value > 0:
+        if self.ncbi_id_valid(value):
             self._ncbi_id = f'txid{value}'
         else:
             raise ValueError("NCBI ID must be a positive integer.")
@@ -22,7 +47,40 @@ class BasicParser:
 
     @organism.setter
     def organism(self, value):
-        if isinstance(value, str) and value.strip():
-            self._organism = value
+        if self.organism_valid(value):
+            self._organism = value.strip()
         else:
-            raise ValueError("Organism name must be a non-empty string.")
+            raise ValueError("Incorrect organism name.")
+
+    def parse(self) -> str | None:
+        Entrez.email = 'Test@example.com'
+        organism = self._ncbi_id if self._ncbi_id is not None else self._organism   # TODO: if bot are None
+
+        handle = Entrez.esearch(
+            db='nuccore',
+            term=f'{organism}[Organism] AND RefSeq[Filter] AND {self.gene}[All Fields] NOT WGS[Filter] NOT plasmid[All Fields]',
+            retmax=self.entries
+        )
+        record = Entrez.read(handle)
+
+        gene_sequence = ''
+
+        for id in record['IdList']:
+            handle = Entrez.efetch(db='nuccore', id=id, rettype='gbwithparts', retmode='text')
+            genomes = SeqIO.parse(handle, 'genbank')
+
+            genome: SeqRecord
+            for genome in genomes:
+
+                gene: SeqFeature
+                for gene in genome.features:
+
+                    if 'gene' in gene.qualifiers:
+
+                        if self.gene in gene.qualifiers['gene'][0]:  # TODO
+                            start = gene.location.start
+                            end = gene.location.end
+                            gene_sequence += f'>{self.gene}GI:{id}|{genome.description}\n{genome.seq[start:end]}\n'
+                            break
+
+        return gene_sequence
